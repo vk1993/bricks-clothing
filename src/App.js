5f0bab3d65a4dd816e523b9f832cb64bb4d4ec28
Visal Rao <visalrao19@gmail.com>
2019-08-23 00:19:25 +0530

    created component AND container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './component/homepage/homepage.comp';
+import Homepage from './container/homepage/homepage.comp';
 
 function App() {
   return (
---
+++ src/container/homepage/homepage.comp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.style.scss'
+import Directory from '../../component/directory/directory.comp';
+
+
+const Homepage = () =>(
+    <div className='homepage'>
+        <Directory></Directory>
+    </div>
+);
+
+export default Homepage;
